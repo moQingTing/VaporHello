0f3a2022a47fd3343b80b7caf3f8d23bbc23cddd
mqt <2510094452@qq.com>
2017-05-03 14:17:21 +0800

    测试

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,20 +2,20 @@ import Vapor
 
 let drop = Droplet()
 
-//drop.get { req in
-//    return try drop.view.make("welcome", [
-//    	"message": drop.localization[req.lang, "welcome", "title"]
-//    ])
-//}
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
 
 //路由HTTP Requests
-//drop.get("/name",":name") { req in
-//    if let name = req.parameters["name"]?.string{
-//        return "Hello \(name)!"
-//    }
-//    
-//    return "Error retrieving parameters."
-//}
+drop.get("/name",":name") { req in
+    if let name = req.parameters["name"]?.string{
+        return "Hello \(name)!"
+    }
+    
+    return "Error retrieving parameters."
+}
 
 drop.get("/view"){req in
     return try drop.view.make("VaporDocumentation.html")
