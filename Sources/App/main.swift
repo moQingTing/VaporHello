65deecc8a3ea00421bc1c17e4d7b9adaf10030ce
mqt <2510094452@qq.com>
2017-05-03 13:56:52 +0800

    Init

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,12 +2,26 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+//drop.get { req in
+//    return try drop.view.make("welcome", [
+//    	"message": drop.localization[req.lang, "welcome", "title"]
+//    ])
+//}
+
+//路由HTTP Requests
+//drop.get("/name",":name") { req in
+//    if let name = req.parameters["name"]?.string{
+//        return "Hello \(name)!"
+//    }
+//    
+//    return "Error retrieving parameters."
+//}
+
+drop.get("/view"){req in
+    return try drop.view.make("VaporDocumentation.html")
 }
 
-drop.resource("posts", PostController())
+
+//drop.resource("posts", PostController())
 
 drop.run()
