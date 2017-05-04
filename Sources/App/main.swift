3b53bb97c16aa53a3efe774417516bf7714745af
mqt <2510094452@qq.com>
2017-05-04 18:24:28 +0800

    演示mysql 链接

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,6 +1,12 @@
 import Vapor
+import VaporMySQL
 
 let drop = Droplet()
+
+//数据库
+let mysqlPro = try VaporMySQL.Provider(host: "localhost", user: "root", password: "123456", database: "mysqlDemo")
+
+drop.addProvider(mysqlPro)
 
 drop.get { req in
     return try drop.view.make("welcome", [
@@ -8,14 +14,22 @@ drop.get { req in
     ])
 }
 
+
+
+
 //路由HTTP Requests
 drop.get("/name",":name") { req in
     if let name = req.parameters["name"]?.string{
-        return "Hello \(name)!"
+        
+        let result = try mysqlPro.driver.mysql("select * from where username='" + name.string! + "';")
+        let userinfo = result[0]
+        return try JSON(node: userinfo)
+    }else{
+        return try JSON(node: ["error" : "error: user name is nil"])
     }
-    
-    return "Error retrieving parameters."
 }
+
+
 
 drop.get("/view"){req in
     return try drop.view.make("VaporDocumentation.html")
